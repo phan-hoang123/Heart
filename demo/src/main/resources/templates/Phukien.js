613327c06022ffc28882a185edd6d34e91dfc308
phan-hoang123 <phanhoang201025@gmail.com>
2025-10-20 00:27:38 +0700

    Delete demo directory

--- demo/src/main/resources/templates/Phukien.js
+++
@@ -1,6 +0,0 @@
-const slider = document.querySelector('.price-slider');
-        const output = document.querySelector('.price-range-value');
-        slider.oninput = function() {
-            // Định dạng tiền tệ Việt Nam cho đẹp
-            output.innerHTML = new Intl.NumberFormat('vi-VN').format(this.value) + '₫';
-        }
